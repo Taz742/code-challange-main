b200dc6a7a4761c4bd081bb1ca922ef54e14609d
Tazo Leladze <t.leladze@omedia.ge>
2021-10-07 21:00:55 +0400

    structurise project

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,19 +1,16 @@
 import React from "react";
 import { Provider } from "mobx-react";
-// import Editor from "rich-markdown-editor";
 
 import stores from "./stores";
 
+import { DocumentsView } from "./components/documents/";
+
 import "./App.css";
-import DocumentsList from "./components/todo-list";
 
 const App: React.FC = () => {
   return (
     <Provider {...stores}>
-      {/* <div style={{ margin: "25px" }}>
-        <Editor defaultValue="Hello world!" />
-      </div> */}
-      <DocumentsList />
+      <DocumentsView />
     </Provider>
   );
 };
